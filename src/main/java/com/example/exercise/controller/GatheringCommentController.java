451b75208ffacd4bb6ce3cddc2fdbd8f8cd9e682
Son Hyeeun <guswlsrkdmf@naver.com>
2023-04-14 12:43:04 +0900

    #34[refactor] 스프링 시큐리티 적용 gatheringController, GatheringCommentController

--- src/main/java/com/example/exercise/controller/GatheringCommentController.java
+++ src/main/java/com/example/exercise/controller/GatheringCommentController.java
@@ -1,12 +1,14 @@
+
 package com.example.exercise.controller;
 
 import com.example.exercise.dto.gathering.GatheringCommentRequestDto;
 import com.example.exercise.dto.gathering.GatheringCommentResponseDto;
-import com.example.exercise.entity.User;
+import com.example.exercise.security.UserDetailsImpl;
 import com.example.exercise.service.gathering.GatheringCommentServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,21 +19,22 @@ public class GatheringCommentController {
     private final GatheringCommentServiceImpl gatheringCommentService;
     //댓글 작성
     @PostMapping
-    public ResponseEntity<GatheringCommentResponseDto> createGatheringComment(@PathVariable Long gatheringId, User user, @RequestBody GatheringCommentRequestDto commentRequestDto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(gatheringCommentService.createGatheringComment(gatheringId,user, commentRequestDto));
+    public ResponseEntity<GatheringCommentResponseDto> createGatheringComment(@PathVariable Long gatheringId, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody GatheringCommentRequestDto commentRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(gatheringCommentService.createGatheringComment(gatheringId,userDetails.getUser(), commentRequestDto));
     }
 
     //댓글 수정
     @PatchMapping("/{gatheringCommentId}")
-    public ResponseEntity<GatheringCommentResponseDto> updateGatheringComment(@PathVariable Long gatheringCommentId, User user,@RequestBody GatheringCommentRequestDto commentRequestDto){
-        return ResponseEntity.status(HttpStatus.OK).body(gatheringCommentService.updateGatheringComment(gatheringCommentId,user,commentRequestDto));
+    public ResponseEntity<GatheringCommentResponseDto> updateGatheringComment(@PathVariable Long gatheringCommentId, @AuthenticationPrincipal UserDetailsImpl userDetails,@RequestBody GatheringCommentRequestDto commentRequestDto){
+        return ResponseEntity.status(HttpStatus.OK).body(gatheringCommentService.updateGatheringComment(gatheringCommentId,userDetails.getUser(),commentRequestDto));
     }
 
     //댓글 삭제
     @DeleteMapping("/{gatheringCommentId}")
-    public void deleteGatheringComment(@PathVariable Long gatheringCommentId, User user){
-        gatheringCommentService.deleteGatheringComment(gatheringCommentId,user);
+    public void deleteGatheringComment(@PathVariable Long gatheringCommentId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        gatheringCommentService.deleteGatheringComment(gatheringCommentId,userDetails.getUser());
     }
 
 }
+
 
